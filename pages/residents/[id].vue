df726265256328fcf04dc805ed4b99b60ead3333
raldincasidar-studio <raldincasidar@gmail.com>
2025-06-12 22:26:59 +0800

    delete fix

--- pages/residents/[id].vue
+++ pages/residents/[id].vue
@@ -117,6 +117,30 @@
           </div>
         </v-card-text>
       </v-card>
+
+          <v-dialog v-model="confirmDeleteDialog" persistent max-width="500px">
+            <v-card>
+              <v-card-title class="text-h5">
+                <v-icon color="error" class="mr-2">mdi-alert-circle-outline</v-icon>
+                Confirm Deletion
+              </v-card-title>
+              <v-card-text>
+                Are you sure you want to permanently delete the record for 
+                <strong>{{ form.first_name }} {{ form.last_name }}</strong>? 
+                <br><br>
+                This action cannot be undone.
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="grey" text @click="confirmDeleteDialog = false" :disabled="deleting">
+                  Cancel
+                </v-btn>
+                <v-btn color="error" variant="flat" @click="deleteResident" :loading="deleting">
+                  Delete Resident
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
     </div>
   </v-container>
 </template>
